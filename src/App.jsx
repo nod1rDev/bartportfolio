af99795e21e1470a1b464099152680cc896beb52
nod1rDev <nodirbeknegmatov2008@gmail.com>
2024-12-22 15:06:22 +0500

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./index.css";
 import Spline from "@splinetool/react-spline";
+import { projects, skills } from "./utils";
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [time, setTime] = useState("");
 
-  const skills = [
-    "AI Integration",
-    "Voice Data",
-    "Chatbots",
-    "Data Collection",
-    "ML",
-    "Business Management",
-    "Development",
-  ];
+  useEffect(() => {
+    const updateTime = () => {
+      const singaporeTime = new Date().toLocaleString("en-US", {
+        timeZone: "Asia/Singapore",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+      });
+      setTime(singaporeTime);
+    };
 
-  const projects = [
-    {
-      title: "Voice Data Collection",
-      description: `High-quality multilingual datasets that enhance AI training efficiency.`,
-      image:
-        "https://files.oaiusercontent.com/file-7CAKWrBerizQKUSXJDjBGD?se=2024-12-22T09%3A10%3A35Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D7165a4fa-ce13-4404-9fec-d2169b4273e6.webp&sig=u50TFsE2OqOt6jrG3v892WRsrcjqkTjB3tHVH2maqEg%3D",
-      link: "https://free-war-fb9.notion.site/Voice-Data-Collection164376631960805580a5e804c7fa6d27?pvs=4",
-    },
-    {
-      title: "Custom Chatbots",
-      description: `Conversational AI solutions tailored for businesses.`,
-      image:
-        "https://www.plego.com/wp-content/uploads/2021/11/chatbot3-1024x652.jpg",
-      link: "https://free-war-fb9.notion.site/Custom-Chatbots-16437663196080d4b8c8e6041d393610?pvs=4",
-    },
-    {
-      title: "Automation Solutions",
-      description: `End-to-end platforms for streamlining operations`,
-      image:
-        "https://media.istockphoto.com/id/1367967285/photo/automation-software-technology-process-system-business-concept.jpg?s=612x612&w=0&k=20&c=VnRZHYgIfInFhaShiStMroW7PnVyvAC4C9SodjBf4ns=",
-      link: "https://free-war-fb9.notion.site/Automation-Solutions1643766319608042ad1ad2f73f880e18?pvs=4 ",
-    },
-    {
-      title: "Website Development",
-      description: `Modern, responsive websites to establish your online presence.`,
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRl_6AuG1TxzQdGmZSN4pF02iLepIl6-4ucQ&s",
-      link: "https://free-war-fb9.notion.site/Website-Development16437663196080059302c01f3c9fe87f?pvs=4",
-    },
-    {
-      title: "Social Media Growth",
-      description: `Strategies and tools to boost engagement andvisibility`,
-      image:
-        "https://recurpost.com/wp-content/uploads/2022/05/good-thing-about-social-media-1024x576.png",
-      link: "https://free-war-fb9.notion.site/Automation-Solutions1643766319608042ad1ad2f73f880e18?pvs=4 ",
-    },
-  ];
+    updateTime();
+    const interval = setInterval(updateTime, 30000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <div className="flex flex-col md:flex-row text-white w-full bg-[#000] h-full md:h-screen gap-4 p-3 md:p-6">
+    <div className="flex flex-col md:flex-row text-white w-full bg-[#000] h-full md:h-screen gap-4 p-2 md:p-6">
       <div className="flex flex-col md:gap-4 w-full md:w-1/4 justify-between">
         <div className="flex-0 h-auto md:h-2/3 flex flex-col border border-[#101010] justify-between bg-[#101010] p-5 rounded-[23px]">
-          <h1 className=" text-3xl md:text-[4.5rem] tracking-tighter   leading-tight md:leading-[5rem] mb-4">
+          <h1 className=" text-[4rem] md:text-[4.5rem] tracking-tighter  leading-[3.6rem]  md:leading-[5rem] mb-4">
             Hello! <br /> I{"'"}m{" "}
-            <span className="font-bold text-4xl md:text-[5.5rem]">Bart</span>
+            <span className="font-bold text-[4rem] md:text-[5.5rem]">Bart</span>
           </h1>
-          <p className="mb-6 text-[#FFFFFFB3] text-[0.8vw] max-w-full md:max-w-[95%] font-[500]">
+          <p className="mb-6 text-[#FFFFFFB3] text-[0.8rem] md:text-[0.8vw] max-w-full md:max-w-[95%] font-[500]">
             The founder of{" "}
             <span className="text-white">ApexBart Solutions</span>. I specialize
             in delivering complete, end-to-end solutions in voice data,
@@ -74,7 +45,7 @@ function App() {
             {skills.map((e) => (
               <span
                 key={e}
-                className="border cursor-pointer text-[0.56vw] border-slate-400 hover:bg-gray-700 hover:bg-opacity-[30%] transition-all duration-700 px-3 py-1 rounded-[24px]"
+                className="border cursor-pointer text-[11px] md:text-[0.56vw] border-slate-400 hover:bg-gray-700 hover:bg-opacity-[30%] transition-all duration-700 px-3 py-1 rounded-[24px]"
               >
                 {e}
               </span>
@@ -91,7 +62,7 @@ function App() {
           <div id="Location" className="p-5">
             <p className="font-bold text-[1.2rem]">Singapore, SG</p>
             <p className="text-[#FFFFFFB3] text-[0.9rem] font-[500]">
-              15:37 GMT+8 Local time
+              {time} GMT+8 Local time
             </p>
           </div>
           <div className="h-full flex items-center justify-center">
@@ -104,12 +75,12 @@ function App() {
         <div className="flex-0 bg-[#101010] border border-[#101010] p-5 rounded-[23px] h-[14%] items-center flex justify-between mt-4 md:mt-0">
           <button
             onClick={() => setIsModalOpen(true)}
-            className="px-5 py-[16px] max-h-[56px] w-[65%] hover:shadow-white shadow-md transition-all duration-700 rounded-[30px] bg-white text-black text-center text-[1.1rem] font-[700]"
+            className="px-5  py-[16px] max-h-[56px] w-[56%] hover:shadow-white shadow-md transition-all duration-700 rounded-[30px] bg-white text-black text-center text-[1.1rem] font-[700]"
           >
             Contact me
           </button>
           <a href="https://free-war-fb9.notion.site/Professional-Portfolio-Bart-16137663196080489062f257bb6773fc?pvs=4">
-            <button className="bg-[#ffffff1a]  max-h-[56px] py-[16px] font-bold text-[0.8rem] rounded-[30px] px-[34px] hover:border hover:border-white transition-all duration-700 hover:bg-opacity-[50%]">
+            <button className="bg-[#ffffff1a] w-[100%] h-[56px]  max-h-[56px] py-[16px] font-bold text-[0.8rem] rounded-[30px] px-[34px] hover:border hover:border-white transition-all duration-700 hover:bg-opacity-[50%]">
               GET MY CV
             </button>
           </a>
@@ -129,8 +100,11 @@ function App() {
       <div className="flex flex-col w-full md:w-1/4 gap-4 justify-between">
         <div
           id="logos"
-          className="flex-0 min-h-[58%] pt-5 md:pb-0 flex flex-col border rounded-[23px] bg-[#101010] border-[#101010] items-center justify-center"
+          className="flex-0 min-h-[60%]  md:pb-0 flex flex-col border rounded-[23px] bg-[#101010] border-[#101010] items-center justify-center"
         >
+          <span className="font-bold mt-4 mb-3  text-[#FFFFFFB3]">
+            Let{"'"}s work together
+          </span>
           <div className="containerr">
             <div className="hexagonArea">
               <div className="hexagon group">
@@ -196,7 +170,7 @@ function App() {
                 </a>
               </div>
             </div>
-            <div className="hexagonArea translate-y-[-48px]">
+            <div className="hexagonArea translate-y-[-40px] md:translate-y-[-48px]">
               <div className="hexagon group">
                 <a href="/qr.png" className="flex flex-col items-center">
                   <img src="/weChat.png" className="w-[48px] h-[48px]" alt="" />
@@ -222,8 +196,12 @@ function App() {
               </div>
             </div>
           </div>
+          <span className="font-bold  translate-y-[-30px] max-w-[90%]  text-[0.8rem] text-[#FFFFFFB3]">
+            Let’s discuss how we can work together to create impactful solutions
+            tailored to your needs.
+          </span>
         </div>
-        <div className="flex-0 min-h-[38%] border rounded-[23px] p-6 bg-[#101010] border-[#101010] overflow-y-auto">
+        <div className="flex-0 min-h-[36%] border rounded-[23px] p-6 bg-[#101010] border-[#101010] overflow-y-auto">
           <h3 className="text-[1.2rem] font-bold text-[#FFFFFF] mb-4">
             Latest projects
           </h3>
@@ -262,8 +240,14 @@ function App() {
       </div>
 
       {isModalOpen && (
-        <div className="fixed framer inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-[#1a1a1a] p-4 md:p-12 rounded-[32px] shadow-lg max-w-[340px] md:max-w-[500px] w-full">
+        <div
+          className="fixed framer inset-0 flex items-center justify-center bg-black bg-opacity-50"
+          onClick={() => setIsModalOpen(false)}
+        >
+          <div
+            className="bg-[#1a1a1a] p-4 md:p-12 rounded-[32px] shadow-lg max-w-[340px] md:max-w-[500px] w-full"
+            onClick={(e) => e.stopPropagation()}
+          >
             <h2 className="text-[2rem] md:text-[5rem] font-bold text-center mb-4">
               Say hi!
             </h2>
@@ -291,8 +275,8 @@ function App() {
               </button>
             </form>
             <button
-              onClick={setIsModalOpen(false)}
-              className="w-full py-2 bg-red-500 text-white backrounded-xl font-bold"
+              onClick={() => setIsModalOpen(false)}
+              className="w-full py-2 bg-red-500 text-white rounded-xl font-bold mt-2"
             >
               Back
             </button>
